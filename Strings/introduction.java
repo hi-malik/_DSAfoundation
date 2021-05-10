7defc48e02b083e28c5914ad61a7ddf3bba59a6d
Himanshu Malik <jsm.28415@gmail.com>
2021-05-10 12:38:32 +0530

    String folder initialized

---
+++ Strings/introduction.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class introduction{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in); 
+        //printing string
+        // String s1 = "Hello";  //learnToDeclare
+        // String s1 = scn.next(); //learnToTakeUserInput
+        // String s1 = scn.nextLine(); //learnToTakeUserInput
+        // String s2 = scn.nextLine(); //learnToTakeUserInput
+        // System.out.println(s1); //learnToTakeUserOutput
+        // System.out.println(s2);
+        
+        // learning to count length
+        // String s = scn.nextLine();
+        // System.out.println(s.length());
+
+        //learnCharat-to get character
+
+        // String s = scn.nextLine();
+        // char ch = s.charAt(5);
+        // System.out.println(ch);
+
+        // for(int i = 0; i < s.length(); i++){
+            // char ch = s.charAt(i);
+            // System.out.println(ch);
+        // }
+
+        // Substring function
+        
+    }
+
+
+}
+
