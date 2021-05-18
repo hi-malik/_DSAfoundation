9fa616be4766f4e73297ea1002730258805ce4ee
Himanshu Malik <jsm.28415@gmail.com>
2021-05-18 14:19:49 +0530

    arrayAll_Answers

--- BasicsOfProgramming/Arrays/findFirstAndLastPositionOfElementInSortedArray.java
+++ BasicsOfProgramming/Arrays/findFirstAndLastPositionOfElementInSortedArray.java
@@ -1,31 +1,51 @@
+import java.io.*;
 import java.util.*;
- 
- class findFirstAndLastPositionOfElementInSortedArray {
- 
-     public static int[] searchRange(int[] nums, int target) {
-         //Write your code here
-     }
- 
-     public static void main(String[] args) {
-         // TODO Auto-generated method stub
-         Scanner sc = new Scanner(System.in);
-         int[] A = new int[sc.nextInt()];
-         for (int i = 0; i < A.length; i++) {
-             A[i] = sc.nextInt();
-         }
-         int[] res = searchRange(A, sc.nextInt());
-         for (int a : res) {
-             System.out.print(a+" ");
-         }
-         System.out.println();
-     }
+
+public class findFirstAndLastPositionOfElementInSortedArray{
+
+public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0; i < n; i++){
+       arr[i] = scn.nextInt();
+    }
+    int data = scn.nextInt();
+
+    int left = 0;
+    int right = arr.length - 1;
+    int fi = -1;
+    while(left <= right){
+       int mid = (left + right) / 2;
+       if(data > arr[mid]){
+         left = mid + 1;
+       } else if(data < arr[mid]){
+         right = mid - 1;
+       } else  {
+          fi = mid;
+          right = mid - 1;
+       }
+    }
+
+    left = 0;
+    right = arr.length - 1;
+    int li = -1;
+    while(left <= right){
+       int mid = (left + right) / 2;
+       if(data > arr[mid]){
+         left = mid + 1;
+       } else if(data < arr[mid]){
+         right = mid - 1;
+       } else  {
+          li = mid;
+          left = mid + 1;
+       }
+    }
+
+    System.out.print(fi + " "); 
+    System.out.print(li + " ");
  }
 
-
-
-
-//Not done, want to reask from pepcoding teachers
-
-
-
-
+}
+                        
+                        
