c8634947ed397121e5d4bc9ce2a841f917a81853
Himanshu Malik <jsm.28415@gmail.com>
2021-05-18 10:49:35 +0530

    HackerRank-443

--- Practice_/Solution.java
+++ Practice_/Solution.java
@@ -8,12 +8,16 @@ public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner scn = new Scanner(System.in);
+        Scanner scn= new Scanner(System.in);
         int n = scn.nextInt();
-        
+        int a = 0;
+        int b = 1;
         for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= i; j++){
-                System.out.print("*\t");
+            for(int j = 1; j <=i; j++){
+                System.out.print(a + "\t");
+                int sum = a + b;
+                a = b;
+                b = sum;
             }
             System.out.println();
         }
