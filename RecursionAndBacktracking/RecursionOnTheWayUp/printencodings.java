39f2cd6e22c7230923a6d6a906ccd876d8ffefb8
Himanshu Malik <jsm.28415@gmail.com>
2021-05-21 21:12:21 +0530

    thisAnswerLeft

---
+++ RecursionAndBacktracking/RecursionOnTheWayUp/printencodings.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class printencodings {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        printEncodings(str, "");
+    }
+
+    public static void printEncodings(String str, String asf) {
+        
+    }
+
+}
+
+// This answer has to be complete
