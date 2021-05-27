8130f855a64bc3ebc6f35f66d99b030275275f88
Himanshu Malik <jsm.28415@gmail.com>
2021-05-27 23:34:51 +0530

    Not_complete

---
+++ DynamicProgramming/TimeAndSpace/mergeTwoSortedArrays.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class mergeTwoSortedArrays {
+
+  public static int[] mergeTwoSortedArrays(int[] a, int[] b){
+    //write your code here
+    
+    return null;
+  }
+
+  public static void print(int[] arr){
+    for(int i = 0 ; i < arr.length; i++){
+      System.out.println(arr[i]);
+    }
+  }
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] a = new int[n];
+    for(int i = 0 ; i < n; i++){
+      a[i] = scn.nextInt();
+    }
+    int m = scn.nextInt();
+    int[] b = new int[m];
+    for(int i = 0 ; i < m; i++){
+      b[i] = scn.nextInt();
+    }
+    int[] mergedArray = mergeTwoSortedArrays(a,b);
+    print(mergedArray);
+  }
+
+}
