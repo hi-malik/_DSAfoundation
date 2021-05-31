805f41c6987ae7c2a76ad699aa0024e3aeb7bb48
Himanshu Malik <jsm.28415@gmail.com>
2021-05-31 22:47:16 +0530

    Answers_

--- DynamicProgramming/TimeAndSpace/mergeTwoSortedArrays.java
+++ DynamicProgramming/TimeAndSpace/mergeTwoSortedArrays.java
@@ -5,8 +5,35 @@ public class mergeTwoSortedArrays {
 
   public static int[] mergeTwoSortedArrays(int[] a, int[] b){
     //write your code here
+
+    int result[] = new int[a.length + b.length];
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    while(i < a.length && j < b.length){
+      if(a[i] < b[j]){
+        result[k] = a[i];
+        i++;
+        k++;
+      }else{
+        result[k] = b[j];
+        j++;
+        k++;
+      }
+    }
+    while(i < a.length){
+      result[k] = a[i];
+      i++;
+      k++;
+    }
+    while(j < b.length){
+      result[k] = b[j];
+      j++;
+      k++;
+    }
     
-    return null;
+    return result;
   }
 
   public static void print(int[] arr){
