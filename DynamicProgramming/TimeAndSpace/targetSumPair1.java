0b713d142f4da2cc8b9491f1ab9771cb7877680d
Himanshu Malik <jsm.28415@gmail.com>
2021-05-31 21:56:35 +0530

    Answers_

--- DynamicProgramming/TimeAndSpace/targetSumPair1.java
+++ DynamicProgramming/TimeAndSpace/targetSumPair1.java
@@ -4,18 +4,16 @@ import java.util.*;
 public class targetSumPair1 {
 
   public static void targetSumPair(int[] arr, int target) {
-    //write your code here
+    // write your code here
     Arrays.sort(arr);
     int li = 0;
     int ri = arr.length - 1;
     while (li < ri) {
       if (arr[li] + arr[ri] < target) {
         li++;
-      }
-      else if (arr[li] + arr[ri] > target) {
+      } else if (arr[li] + arr[ri] > target) {
         ri--;
-      }
-      else {
+      } else {
         System.out.println(arr[li] + ", " + arr[ri]);
         li++;
         ri--;
@@ -23,11 +21,12 @@ public class targetSumPair1 {
     }
 
   }
+
   public static void main(String[] args) throws Exception {
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
     int[] arr = new int[n];
-    for (int i = 0 ; i < n; i++) {
+    for (int i = 0; i < n; i++) {
       arr[i] = scn.nextInt();
     }
     int target = scn.nextInt();
