c1225355998d2e0b6d49b32d0f9ab4e1acbf0be7
Himanshu Malik <jsm.28415@gmail.com>
2021-06-13 17:14:14 +0530

    Answer_

---
+++ DynamicProgramming/Dynamic Programming and Greedy/buyAndSellStockonetransactionallowed.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class buyAndSellStockonetransactionallowed {
+    public static void main(String[] args) throws Exception {
+        // write your code here 
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int prices[]= new int[n];
+
+        for(int i = 0; i < prices.length; i++){
+            prices[i] = scn.nextInt();
+        }
+
+        int lsf = Integer.MAX_VALUE; // least price so far
+        int op = 0; // Original price
+        int pist = 0; // Price if sold today
+
+        for(int  i = 0; i < prices.length; i++){
+            if(prices[i] < lsf){
+                lsf = prices[i];
+            }
+
+            pist = prices[i] - lsf;
+
+            if(pist > op){
+                op = pist;
+            }
+        }
+        System.out.println(op);
+    }
+}
