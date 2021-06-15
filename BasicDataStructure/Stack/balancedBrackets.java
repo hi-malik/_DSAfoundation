bfae363a4c755179223bf8c70fb207c6e9cc02aa
Himanshu Malik <jsm.28415@gmail.com>
2021-06-15 23:44:39 +0530

    AnotherWay_ToSolveThis

--- BasicDataStructure/Stack/balancedBrackets.java
+++ BasicDataStructure/Stack/balancedBrackets.java
@@ -1,3 +1,65 @@
+// One way to solve -1
+
+// import java.io.*;
+// import java.util.*;
+// 
+// public class balancedBrackets {
+// 
+//   public static void main(String[] args) throws Exception {
+    // Scanner scn = new Scanner(System.in);
+    // String str = scn.nextLine();
+    // Stack<Character> st = new Stack<>();
+// 
+    // for (int i = 0; i < str.length(); i++) {
+    //   char ch = str.charAt(i);
+    //   if (ch == '[' || ch == '(' || ch == '{') {
+        // st.push(ch);
+    //   }
+    //   else if (ch == ']') {
+        // boolean val = handleClosing(st, '[');
+        // if(val == false){
+            // System.out.println(val);
+            // return;
+        // }
+    //   }
+    //   else if (ch == ')') {
+        // boolean val = handleClosing(st, '(');
+        // if(val == false){
+            // System.out.println(val);
+            // return;
+        // }
+    //   }
+    //   else if (ch == '}') {
+        // boolean val = handleClosing(st, '{');
+        // if(val == false){
+            // System.out.println(val);
+            // return;
+        // }
+    //   }
+    // }
+    // if (st.size() == 0) {
+    //   System.out.println(true);
+    // }
+    // else {
+    //   System.out.println(false);
+    // }
+//   }
+//   public static boolean handleClosing(Stack<Character> st, char chorospond) {
+    // if (st.size() == 0) {
+    //   return false;
+    // }
+    // else if (st.peek() != chorospond) {
+    //   return false;
+    // }
+    // else {
+    //   st.pop();
+    //   return true;
+    // }
+//   }
+// }
+
+// Second way to solve - 2
+
 import java.io.*;
 import java.util.*;
 
@@ -14,24 +76,45 @@ public class balancedBrackets {
         st.push(ch);
       }
       else if (ch == ']') {
-        boolean val = handleClosing(st, '[');
-        if(val == false){
-            System.out.println(val);
-            return;
+        if (st.size() == 0) {
+          System.out.println(false);
+          return;
+        }
+        else if (st.peek() != '[') {
+          System.out.println(false);
+          return;
+        }
+        else {
+          st.pop();
+          // return true;
         }
       }
       else if (ch == ')') {
-        boolean val = handleClosing(st, '(');
-        if(val == false){
-            System.out.println(val);
-            return;
+        if (st.size() == 0) {
+          System.out.println(false);
+          return;
+        }
+        else if (st.peek() != '(') {
+          System.out.println(false);
+          return;
+        }
+        else {
+          st.pop();
+          // return true;
         }
       }
       else if (ch == '}') {
-        boolean val = handleClosing(st, '{');
-        if(val == false){
-            System.out.println(val);
-            return;
+        if (st.size() == 0) {
+          System.out.println(false);
+          return;
+        }
+        else if (st.peek() != '{') {
+          System.out.println(false);
+          return;
+        }
+        else {
+          st.pop();
+          // return true;
         }
       }
     }
@@ -42,17 +125,4 @@ public class balancedBrackets {
       System.out.println(false);
     }
   }
-  public static boolean handleClosing(Stack<Character> st, char chorospond) {
-    if (st.size() == 0) {
-      return false;
-    }
-    else if (st.peek() != chorospond) {
-      return false;
-    }
-    else {
-      st.pop();
-      return true;
-    }
-  }
 }
-
