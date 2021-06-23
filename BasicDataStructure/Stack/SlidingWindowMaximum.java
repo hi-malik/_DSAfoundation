b786d6c8f174097a3f6a715ec6b9ead125f2e6bd
Himanshu Malik <jsm.28415@gmail.com>
2021-06-23 21:36:17 +0530

    Answer_

---
+++ BasicDataStructure/Stack/SlidingWindowMaximum.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class SlidingWindowMaximum{
+  
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for(int i = 0; i < n; i++){
+       a[i] = Integer.parseInt(br.readLine());
+    }
+    int k = Integer.parseInt(br.readLine());
+
+    // code
+    
+    int nge[] = new int[a.length]; //Next greater element
+    Stack<Integer> st = new Stack<>();
+    st.push(a.length - 1);
+    nge[a.length - 1] = a.length;
+    
+    for(int i = a.length - 2; i >= 0; i--){
+        while(st.size() > 0 && a[i] >= a[st.peek()]){
+            st.pop();
+        }
+        
+        if(st.size() == 0){
+            nge[i] = a.length;
+        }
+        else{
+            nge[i] = st.peek();
+        }
+        st.push(i);
+    }
+    
+    int j = 0;
+    for(int i = 0; i <= a.length - k; i++){
+        // enter the loop to find the maximum of window starting at i
+        
+        if(j < i){
+            j = i;
+        }
+        
+        while(nge[j] < i + k){
+            j = nge[j];
+        }
+        
+        System.out.println(a[j]);
+    }
+ }
+}
