04a97ee093d7fb6ce239bca1859dd398b34b2f06
Himanshu Malik <jsm.28415@gmail.com>
2021-06-25 21:23:03 +0530

    reference and instance understanding (called as Object)

--- BasicDataStructure/Stack/oopsClasses.java
+++ BasicDataStructure/Stack/oopsClasses.java
@@ -19,5 +19,11 @@ public class oopsClasses{
         p2.age = 20;
         p2.name = "World";
         p2.sayHi();
+
+        person p3 = p1; // the p3 will copy all of p1 info
+        p3.age = 40;
+        p3.name = "HW";
+        p3.sayHi(); //now what will happens over here is that, p3 is referencing the same instance
+        p1.sayHi(); // so p1 and p3 now are both on same address
     }
 }
