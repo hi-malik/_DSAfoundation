a68687d9193ab81daf3cc76a6eae90204857cac5
Himanshu Malik <jsm.28415@gmail.com>
2021-06-29 13:27:43 +0530

    MaxOfArray

---
+++ Practice_/revision/ra3.java
@@ -0,0 +1,33 @@
+package revision;
+
+import java.io.*;
+import java.util.*;
+
+public class ra3 {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = scn.nextInt();
+        }
+        int max = maxOfArray(arr, 0);
+        System.out.println(max);
+    }
+
+    public static int maxOfArray(int[] arr, int idx){
+        if(idx == arr.length){
+            return idx;
+        }
+        int faith = maxOfArray(arr, idx + 1);
+        if(faith > arr[idx]){
+            return faith;
+        }
+        else{
+            return arr[idx];
+        }
+    }
+
+}
