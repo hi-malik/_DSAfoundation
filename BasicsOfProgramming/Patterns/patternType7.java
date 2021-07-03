a0fc5726e32feda986e5cbb79f94b80e888310c5
Himanshu Malik <jsm.28415@gmail.com>
2021-07-03 11:43:29 +0530

    nothing

--- BasicsOfProgramming/Patterns/patternType7.java
+++ BasicsOfProgramming/Patterns/patternType7.java
@@ -6,17 +6,30 @@ public class patternType7 {
         Scanner scn = new Scanner(System.in);
 
         // write ur code here
+        // int n = scn.nextInt();
+        // 
+        // for(int i = 1; i <= n; i++){
+            // for(int j = 1; j <= i; j++){
+                // if( j == i ){
+                    // System.out.print("*\t");
+                // }else{
+                    // System.out.print("\t");
+                // }
+            // }
+            // System.out.println();
+        // }
         int n = scn.nextInt();
-        
+        int stars = 1;
+        int spaces = 0;
         for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= i; j++){
-                if( j == i ){
-                    System.out.print("*\t");
-                }else{
-                    System.out.print("\t");
-                }
+            for(int j = 1; j <= stars; j++){
+                System.out.print("*\t");
+            }
+            for(int j = 1; j <= spaces; j++){
+                System.out.print("\t");
             }
             System.out.println();
+            spaces++;
         }
 
     }
