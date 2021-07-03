4a51d6e31fae80a833e35b6644867443ca91e599
Himanshu Malik <jsm.28415@gmail.com>
2021-07-03 11:48:11 +0530

    one more way to solve this

--- BasicsOfProgramming/Patterns/patternType8.java
+++ BasicsOfProgramming/Patterns/patternType8.java
@@ -7,16 +7,27 @@ public class patternType8 {
 
         // write ur code here
         int n = scn.nextInt();
+        // for(int i = 1; i <= n; i++){
+            // for(int j = 1; j <= n; j++){
+                // if( i + j == n + 1 ){
+                    // System.out.print("*\t");
+                // }
+                // else{
+                    // System.out.print("\t");
+                // }
+            // }
+            // System.out.println();
+        // }
+                    //  One more way to solve this
+        int spaces = n - 1;
+        int stars = 1;
         for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                if( i + j == n + 1 ){
-                    System.out.print("*\t");
-                }
-                else{
-                    System.out.print("\t");
-                }
+            for(int j = 1; j <= spaces; j++){
+                System.out.print("\t");
             }
+            System.out.print("*\t");
             System.out.println();
+            spaces--;
         }
 
     }
