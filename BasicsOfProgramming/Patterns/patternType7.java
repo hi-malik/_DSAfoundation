ffe2ecdf03e018cb810ae9b3a2647364a47f1be7
Himanshu Malik <jsm.28415@gmail.com>
2021-07-03 12:00:29 +0530

    I solve it with one more way

--- BasicsOfProgramming/Patterns/patternType7.java
+++ BasicsOfProgramming/Patterns/patternType7.java
@@ -22,7 +22,7 @@ public class patternType7 {
         int stars = 1;
         int spaces = 0;
         for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= stars; j++){
+            for(int j = 1; j <= spaces; j++){
                 System.out.print("\t");
             }
             System.out.print("*\t");
