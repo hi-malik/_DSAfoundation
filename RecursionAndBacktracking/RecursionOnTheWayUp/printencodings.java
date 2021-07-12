798b64f58aff6fd6e84193b4faa25077ddb799a7
Himanshu Malik <jsm.28415@gmail.com>
2021-07-12 11:08:14 +0530

    It's completed

--- RecursionAndBacktracking/RecursionOnTheWayUp/printencodings.java
+++ RecursionAndBacktracking/RecursionOnTheWayUp/printencodings.java
@@ -10,9 +10,39 @@ public class printencodings {
     }
 
     public static void printEncodings(String str, String asf) {
-        
+        if (str.length() == 0) {
+            System.out.println(asf);
+            return;
+        } else if (str.length() == 1) {
+            char ch = str.charAt(0);
+            if (str.length() == 0) {
+                return;
+            } else {
+                int chv = ch - '0';
+                char code = (char) ('a' + chv - 1);
+                System.out.println(asf + code);
+            }
+        } else {
+            char ch = str.charAt(0);
+            String ros = str.substring(1);
+            if (ch == '0') {
+                return;
+            } else {
+                int chv = ch - '0';
+                char code = (char) ('a' + chv - 1);
+                printEncodings(ros, asf + code);
+            }
+            String ch1 = str.substring(0, 2);
+            String ros12 = str.substring(2);
+
+            int ch12v = Integer.parseInt(ch1);
+            if (ch12v <= 26) {
+                char code = (char) ('a' + ch12v - 1);
+                printEncodings(ros12, asf + code);
+            }
+        }
     }
 
 }
 
-// This answer has to be complete
+// This answer is completed on 12th July 2021
