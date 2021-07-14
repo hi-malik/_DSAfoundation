cd8daee382925763a18d294170bf3d7cf3b6cd32
Himanshu Malik <jsm.28415@gmail.com>
2021-07-14 09:15:52 +0530

    Completed

--- BasicsOfProgramming/2dArrays/wakanda2.java
+++ BasicsOfProgramming/2dArrays/wakanda2.java
@@ -5,6 +5,20 @@ public class wakanda2 {
 
     public static void main(String[] args) throws Exception {
         // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int arr[][] = new int[n][n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        
+        for(int gap = 0; gap < arr.length; gap++){
+            for(int i = 0, j =gap; j < arr.length; i++, j++){
+                System.out.println(arr[i][j]);
+            }
+        }
     }
 
 }
