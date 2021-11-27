d541bf0fefa761eb8af6a0b8edae13af4a638081
Himanshu Malik <jsm.28415@gmail.com>
2021-11-27 08:54:44 +0530

    Another test

--- Practice_/revision2/git.java
+++ Practice_/revision2/git.java
@@ -1,24 +1,35 @@
-// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+// A k-mirror number is a positive integer without leading zeros that reads the same both forward and backward in base-10 as well as in base-k.
 
-// Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+// For example, 9 is a 2-mirror number. The representation of 9 in base-10 and base-2 are 9 and 1001 respectively, which read the same both forward and backward.
+// On the contrary, 4 is not a 2-mirror number. The representation of 4 in base-2 is 100, which does not read the same both forward and backward.
+// Given the base k and the number n, return the sum of the n smallest k-mirror numbers.
 
-// Input: x = 123
-// Output: 321
+// Input: k = 2, n = 5
+// Output: 25
+// Explanation:The 5 smallest 2-mirror numbers and their representations in base-2 are listed as follows:
+//   base-10    base-2
+//   1          1
+//   3          11
+//   5          101
+//   7          111
+//   9          1001
+// Their sum = 1 + 3 + 5 + 7 + 9 = 25. 
 
 class Solution {
-    public int reverse(int x) {
-        int result = 0;
-        while (x != 0) {
-            int pop = x % 10;
-            x /= 10;
-            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && pop > 7)) {
-                return 0;
+    public long kMirror(int k, int n) {
+        int sum = 0;
+
+        for(int i = 1; i <= n; i++) {
+            int num = i;
+            int count = 0;
+            while(num != 0) {
+                count += num % k;
+                num /= k;
             }
-            if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && pop < -8)) {
-                return 0;
+            if(count == k) {
+                sum += i;
             }
-            result = result * 10 + pop;
         }
-        return result;        
-    }
+        return sum;
+    }   
 }
