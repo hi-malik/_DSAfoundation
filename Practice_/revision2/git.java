3d239387f4987c85308ccf7ba346ee5ddad3881d
Himanshu Malik <jsm.28415@gmail.com>
2021-11-28 11:46:01 +0530

    fail

--- Practice_/revision2/git.java
+++ Practice_/revision2/git.java
@@ -1,35 +1,56 @@
-// A k-mirror number is a positive integer without leading zeros that reads the same both forward and backward in base-10 as well as in base-k.
+// You are given a 0-indexed array of distinct integers nums.
 
-// For example, 9 is a 2-mirror number. The representation of 9 in base-10 and base-2 are 9 and 1001 respectively, which read the same both forward and backward.
-// On the contrary, 4 is not a 2-mirror number. The representation of 4 in base-2 is 100, which does not read the same both forward and backward.
-// Given the base k and the number n, return the sum of the n smallest k-mirror numbers.
+// There is an element in nums that has the lowest value and an element that has the highest value. We call them the minimum and maximum respectively. Your goal is to remove both these elements from the array.
 
-// Input: k = 2, n = 5
-// Output: 25
-// Explanation:The 5 smallest 2-mirror numbers and their representations in base-2 are listed as follows:
-//   base-10    base-2
-//   1          1
-//   3          11
-//   5          101
-//   7          111
-//   9          1001
-// Their sum = 1 + 3 + 5 + 7 + 9 = 25. 
+// A deletion is defined as either removing an element from the front of the array or removing an element from the back of the array.
+
+// Return the minimum number of deletions it would take to remove both the minimum and maximum element from the array.
+
+// Input: nums = [2,10,7,5,4,1,8,6]
+// Output: 5
+// Explanation: 
+// The minimum element in the array is nums[5], which is 1.
+// The maximum element in the array is nums[1], which is 10.
+// We can remove both the minimum and maximum by removing 2 elements from the front and 3 elements from the back.
+// This results in 2 + 3 = 5 deletions, which is the minimum number possible.
+
+// Input: nums = [0,-4,19,1,8,-2,-3,5]
+// Output: 3
+// Explanation: 
+// The minimum element in the array is nums[1], which is -4.
+// The maximum element in the array is nums[2], which is 19.
+// We can remove both the minimum and maximum by removing 3 elements from the front.
+// This results in only 3 deletions, which is the minimum number possible.
+
+// Input: nums = [101]
+// Output: 1
+// Explanation:  
+// There is only one element in the array, which makes it both the minimum and maximum element.
+// We can remove it with 1 deletion.
 
 class Solution {
-    public long kMirror(int k, int n) {
-        int sum = 0;
-
-        for(int i = 1; i <= n; i++) {
-            int num = i;
-            int count = 0;
-            while(num != 0) {
-                count += num % k;
-                num /= k;
+    public int minimumDeletions(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] < min){
+                min = nums[i];
             }
-            if(count == k) {
-                sum += i;
+            if(nums[i] > max){
+                max = nums[i];
             }
         }
-        return sum;
-    }   
-}
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == min){
+                count++;
+            }
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == max){
+                count++;
+            }
+        }
+        return count;
+    }
+            
